404b20b61db4810e6df692eb81f1776e41e72531
zhu18 <zhu18@vip.qq.com>
2022-05-25 20:30:40 +0800

    remove node_modules dir

--- node_modules/rgbaster/dist/helpers.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const getContext: (width: any, height: any) => CanvasRenderingContext2D;
-export declare const getImageData: (src: string, scale?: number) => Promise<Uint8ClampedArray>;
-export declare const getCounts: (data: Uint8ClampedArray, ignore: string[]) => [];
